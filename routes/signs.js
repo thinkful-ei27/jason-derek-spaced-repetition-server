42d7180a503600124f0ccbb66fc41280b5ace913
Derek Houck <derek@derekhouck.com>
2019-03-19 10:02:55 -0700

    Hot fix for GET /api/signs

--- routes/signs.js
+++ routes/signs.js
@@ -4,7 +4,7 @@ const signs = require('../db/signs');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.json({ sign: `${req.protocol}://${req.get('host')}/assets/${signs[0]}` });
+  res.json({ sign: `${req.protocol}://${req.get('host')}/assets/${signs[0].sign}` });
 });
 
 module.exports = router;
